35d76506a64b36dc2188e149751522dc8050ecd6
Syed Adil Wafa <50646069+syed-adil-wafa@users.noreply.github.com>
2020-09-09 12:09:00 -0400

    add target_class

---
+++ helper-functions/target_class.m
@@ -0,0 +1,14 @@
+% Define classification targets for multi-electrode array wells
+
+function [target_class] = target_class(conditions, number_of_conditions)
+
+target_class = [];
+for num = 1:number_of_conditions
+    
+    condition = conditions(num, :);
+    condition_length = length(condition(~isnan(condition)));
+    class(:, 1:condition_length) = num;
+    target_class = [target_class; class'];
+    class = [];
+    
+end
